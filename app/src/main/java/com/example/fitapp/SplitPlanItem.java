18e030ea6a272c29d67178115fad44e695dd0f6d
Lufthanse <Hannes.Kilger@t-online.de>
2021-02-11 22:34:17 +0100

    11.02.2021 22:33
    Layout idea create new split plan

--- app/src/main/java/com/example/fitapp/SplitPlanItem.java
+++ app/src/main/java/com/example/fitapp/SplitPlanItem.java
@@ -1,11 +1,16 @@
 package com.example.fitapp;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
 public class SplitPlanItem {
     private int mImageResource;
     private String mText1;
     private String mText2;
+    private Map<String, Exercise> splitDays = new HashMap<String, Exercise>();
 
-    public SplitPlanItem(int mImageResource, String mText1, String mText2){
+    public SplitPlanItem(int mImageResource, String mText1, String mText2  ){
         this.mImageResource = mImageResource;
         this.mText1 = mText1;
         this.mText2 = mText2;
